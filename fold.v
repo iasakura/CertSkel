4fef74f6311d494f32190eecc37c48276ae57f81
Izumi ASAKURA <i.asakura58@gmail.com>
2015-03-30 04:13:59 +0900

    proving lemma about bijective assertions

--- fold.v
+++ fold.v
@@ -56,8 +56,6 @@ Section Fold.
   Notation "e1 '-->p' ( p ,  e2 )" := (Apointsto p e1 e2) (at level 75) : assn_scope.
   Delimit Scope assn_scope with assn.
 
-  Variable f : nat -> Z.
-
   Definition nat_of_fin (i : Fin.t ntrd) : nat := projT1 (Fin.to_nat i).
   Definition Z_of_fin (i : Fin.t ntrd) : Z := Z.of_nat (nat_of_fin i).
 
@@ -131,13 +129,77 @@ Section Fold.
     Proof.
       cutrewrite (fst default = snd default); [tauto | unfold default; eauto].
     Qed.
-
+    Import VectorNotations.
+    Lemma swap_wf : 
+      forall (m : nat) (s : stack) (h : pheap) (p : assn) (ps : Vector.t assn m) (i : Fin.t m),
+        Aistar_v (p :: ps) s h -> Aistar_v (ps[@i] :: (Vector.replace ps i p)) s h.
+    Proof.
+      induction m; intros s h p ps i.
+      - rewrite (vinv0 ps); inversion i.
+      - destruct (vinvS ps) as (p' & ps' & ?); subst. 
+        destruct (finvS i) as [|[i' ?]]; subst; [simpl |].
+        + intros (ph1 & ph2 & Hp1 & (ph3 & ph4 & Hp3 & Hp4 & Hd34 & Heq34) & Hdis12 & Heq12).
+          assert (pdisj ph1 ph4) as Hdis14.
+          { rewrite <-Heq34 in Hdis12; apply pdisjE2 in Hdis12; eauto. }
+          exists ph3, (phplus_pheap Hdis14); repeat split; eauto.
+          * exists ph1, ph4; repeat split; eauto.
+          * simpl; apply pdisj_padd_comm; [rewrite Heq34|]; eauto.
+          * simpl; rewrite padd_left_comm, Heq34; eauto.
+            apply pdisj_padd_comm; [rewrite Heq34|]; eauto.
+        + intros (ph1 & ph2 & Hp1 & (ph3 & ph4 & Hp3 & Hp4 & Hd34 & Heq34) & Hdis12 & Heq12); simpl.
+          assert (pdisj ph1 ph4) as Hdis14.
+          { rewrite <-Heq34 in Hdis12; apply pdisjE2 in Hdis12; eauto. }
+          assert (Aistar_v (p :: ps') s (phplus_pheap Hdis14)) as H14.
+          { simpl; repeat eexists; eauto. }
+          destruct (IHm _ _ _ _ i' H14) as (ph1' & ph4' & H1' & H4' & Hdis14' & Heq14').
+          simpl in Heq14'.
+          assert (pdisj ph3 (phplus ph1' ph4')) as Hdis314'.
+          { rewrite <-Heq34 in Hdis12. apply pdisj_padd_comm in Hdis12; eauto.
+            rewrite <-Heq14' in Hdis12; eauto. }
+          assert (pdisj ph3 ph4') as Hdis34'.
+          { apply pdisjE2 in Hdis314'; eauto. }
+          exists ph1', (phplus_pheap Hdis34'); repeat split; eauto.
+          exists ph3, ph4'; repeat split; eauto.
+          * simpl; apply pdisj_padd_comm; eauto.
+          * simpl. rewrite padd_left_comm, Heq14', padd_left_comm, Heq34; eauto.
+            apply pdisj_padd_comm; [rewrite Heq34; eauto | eauto].
+    Qed.
+          
+    Lemma biject_wf : forall (m : nat) (s : stack) (h : pheap) (ps qs : Vector.t assn m) 
+          (f : Fin.t m -> Fin.t m) (g : Fin.t m -> Fin.t m),
+      (forall i : Fin.t m, f (g i) = i /\ g (f i) = i) ->
+      qs = init (fun i : Fin.t m => ps[@(f i)]) ->
+      (Aistar_v ps s h -> Aistar_v qs s h).
+    Proof.
+      induction m; intros s h ps qs f g Hbi HeqQP.
+      - rewrite (vinv0 ps), (vinv0 qs); simpl; tauto.
+      - destruct (vinvS ps) as (p & ps' & ?), (vinvS qs) as (q & qs' & ?); subst; intros Hps.
+        assert (f (g Fin.F1) = Fin.F1) as Heq1 by (destruct (Hbi Fin.F1) as [? ?]; congruence).
+        assert (init (fun i : Fin.t (S m) => (p :: ps')[@f i]) =
+                (p :: ps')[@f Fin.F1] :: init (fun i : Fin.t m => (p :: ps')[@f (Fin.FS i)])) as Heq.
+        { apply eq_nth_iff; intros p1 p2 ?; subst; rewrite !init_spec.
+          destruct (finvS p2) as [|[p' ?]]; subst; eauto.
+          simpl; rewrite init_spec; eauto. }
+        rewrite Heq; clear Heq.
+        destruct (finvS (f Fin.F1)) as [H1 | [i' H1]].
+        destruct Hps as (h1 & h2 & Hsat1 & Hsat2 & Hdis12 & Heq12).
+        assert (forall i : Fin.t m, exists fSi : Fin.t m,
+                  f (Fin.FS i) = Fin.FS fSi).
+        { intros i; destruct (finvS (f (Fin.FS i))) as [H |]; eauto.
+          destruct (Hbi (Fin.FS i)) as [_ Hgf].
+          rewrite H in Hgf.
+          destruct (Hbi (Fin.F1)) as [_ Hgf1].
+          rewrite H1 in Hgf1. 
+          rewrite Hgf in Hgf1; inversion Hgf1. }
+        apply vec_exvec in H as [rs Hrs].
+        
     Lemma barrier_wf (i : nat) (st : pstate) :
        Aistar_v (fst (bspec i)) (fst st) (snd st) <-> Aistar_v (snd (bspec i)) (fst st) (snd st).
     Proof.
       destruct i; simpl; [|apply default_wf].
-      assert (
-
+      unfold bpre, bpost, Z_of_fin, nat_of_fin. induction ntrd.
+      - split; simpl; intros H x; apply H.
+      - 
   Section FoldDef.
     Definition fold :=
       r ::= n ;;
