9c7f980afe2cf0231cf9d7de3e07a2a382d4a94d
Izumi ASAKURA <i.asakura58@gmail.com>
2015-10-13 01:23:14 +0900

    change statements of lemmas for assertion to fit changes

--- assertion_lemmas.v
+++ assertion_lemmas.v
@@ -36,13 +36,15 @@ Definition Adisj (P1 P2 : assn) : assn := (nosimpl (fun (s : stack) (ph : pheap)
 Notation "P '\\//' Q" := (Adisj P Q) (at level 85, right associativity).
 Definition Apure (p : Prop) := (nosimpl (fun (s : stack) (ph : pheap) => p)).
 Notation pure p := (Apure p).
-Definition Apointsto (p : Qc) (e1 : _exp) (e2 : exp) := (nosimpl (fun (s : stack) (ph : pheap) =>
+Definition Apointsto (p : Qc) (e1 : loc_exp) (e2 : exp) := (nosimpl (fun (s : stack) (ph : pheap) =>
   forall x, this ph x = if loc_eq_dec x (ledenot e1 s) then Some (p, edenot e2 s) else None)).
-Notation "e1 '-->sh' ( p ,  e2 )" := (Apointsto p e1 e2) (at level 75).
+Notation "e1 '-->p' ( p ,  e2 )" := (Apointsto p e1 e2) (at level 75).
 Definition ban (P : assn) := (nosimpl (emp //\\ P)).
 Notation "!( P )" := (ban P).
 Definition eeq (x y : exp) := (nosimpl (fun s (h : pheap) => edenot x s = edenot y s)).
 Notation "x '===' y" := (eeq x y) (at level 70, no associativity).
+Definition leeq (x y : loc_exp) := (nosimpl (fun s (h : pheap) => ledenot x s = ledenot y s)).
+Notation "x '===l' y" := (leeq x y) (at level 70, no associativity).
 Definition AEx {T : Type} (Px : T -> assn) := (nosimpl (fun s h => ex (fun x => Px x s h))).
 Notation "'Ex' x .. y , p" := (AEx (fun x => .. (AEx (fun y => p)) ..))
   (at level 200, x binder, right associativity).                               
@@ -55,12 +57,12 @@ Coercion bexp_to_assn : bexp >-> assn.
 Delimit Scope assn_scope with assn.
 Delimit Scope exp_scope with exp.
 
-Fixpoint is_array (e : exp) (n : nat) (f : nat -> Z) :=
-  match n with
-    | 0 => Aemp
-    | S n' => e + Enum (Z.of_nat n') -->p (1%Qc, Enum (f n')) **
-                       is_array e n' f
-  end.
+(* Fixpoint is_array (e : exp) (n : nat) (f : nat -> Z) := *)
+(*   match n with *)
+(*     | 0 => Aemp *)
+(*     | S n' => e + Enum (Z.of_nat n') -->p (1%Qc, Enum (f n')) ** *)
+(*                        is_array e n' f *)
+(*   end. *)
 
 Notation sat ss P := (P (fst ss) (snd ss)).
 
@@ -70,10 +72,10 @@ Notation nat_of_fin i := (proj1_sig (Fin.to_nat i)).
 Notation Z_of_fin i := (Z.of_nat (nat_of_fin i)).
 
 Ltac unfold_conn_all :=
-  unfold Aemp, Astar, Aconj, Adisj, Apure, Apointsto, ban, eeq, bexp_to_assn in *.
+  unfold Aemp, Astar, Aconj, Adisj, Apure, Apointsto, ban, eeq, leeq, bexp_to_assn in *.
 
 Ltac unfold_conn_in1 H :=
-  unfold Aemp, Astar, Aconj, Adisj, Apure, Apointsto, ban, eeq, bexp_to_assn in H.
+  unfold Aemp, Astar, Aconj, Adisj, Apure, Apointsto, ban, eeq, leeq, bexp_to_assn in H.
 Ltac unfold_conn_in H :=
   match H with
     | (?Hs, ?H) => unfold_conn_in Hs; unfold_conn_in1 H
@@ -81,7 +83,7 @@ Ltac unfold_conn_in H :=
   end.
 
 Ltac unfold_conn :=
-     unfold Aemp, Astar, Aconj, Adisj, Apure, Apointsto, ban, eeq, bexp_to_assn.
+  unfold Aemp, Astar, Aconj, Adisj, Apure, Apointsto, ban, eeq, leeq, bexp_to_assn.
 
 Notation "P |= Q" := (forall s h, P s h -> Q s h) (at level 87).
 
@@ -259,8 +261,8 @@ Proof.
   exists x, x0; split; firstorder.
 Qed.
 
-Lemma mapsto_rewrite1 (E1 E2 E3 : exp) (p : Qc) (s : stack) (h : pheap) :
-  (E1 === E2) s emp_ph -> (E1 -->p (p, E3)) s h -> (E2 -->p (p, E3)) s h.
+Lemma mapsto_rewrite1 (E1 E2 : loc_exp) (E3 : exp) (p : Qc) (s : stack) (h : pheap) :
+  (E1 ===l E2) s emp_ph -> (E1 -->p (p, E3)) s h -> (E2 -->p (p, E3)) s h.
 Proof.
   intros.
   unfold_conn_all; simpl in *.
@@ -268,7 +270,7 @@ Proof.
   auto.
 Qed.
   
-Lemma mapsto_rewrite2 (E1 E2 E3 : exp) (p : Qc) (s : stack) (h : pheap) :
+Lemma mapsto_rewrite2 (E1 : loc_exp) (E2 E3 : exp) (p : Qc) (s : stack) (h : pheap) :
   (E2 === E3) s emp_ph -> (E1 -->p (p, E2)) s h -> (E1 -->p (p, E3)) s h.
 Proof.
   intros.
