41426a53b33d0d80a1c7e32508fbc59b28eeb87c
Izumi ASAKURA <i.asakura58@gmail.com>
2016-02-17 21:24:22 +0900

    STENCIL: prove till correctness of block execution

--- Examples/Stencil.v
+++ Examples/Stencil.v
@@ -2,17 +2,8 @@ Require Import GPUCSL.
 Require Import scan_lib.
 Require Import LibTactics Psatz.
 
-Section Stencil_seq.
+Section Stencil.
 Coercion Var : string >-> var.
-(* Local Notation smem := (Var "smem"). *)
-(* Local Notation arr := (Var "arr"). *)
-(* Local Notation out := (Var "out"). *)
-(* Local Notation t0 := (Var "t0"). *)
-(* Local Notation t1 := (Var "t1"). *)
-(* Local Notation t2 := (Var "t2"). *)
-(* Local Notation tid := (Var "tid"). *)
-(* Local Notation bid := (Var "bid"). *)
-
 Variable ntrd nblk : nat.
 Hypothesis ntrd_neq_0 : ntrd <> 0.
 Hypothesis nblk_neq_0 : nblk <> 0.
@@ -77,7 +68,7 @@ Lemma stencil_ok_th (j : Fin.t nblk) (i : Fin.t ntrd) :
     stencil 
     (is_array_p (Gl aloc) nt_gr f 0 (perm_n nt_gr) **
      (Gl oloc +o Zn (gi i j) -->p (1, (f' (gi i j) + f' (gi i j + 1) + f' (gi i j + 2)))%Z) **
-     is_array_p (Sh sloc) (ntrd + 2) (fun k => f' (k + nf j * ntrd)) 0 (perm_n ntrd)).
+     is_array_p (Sh "smem") (ntrd + 2) (fun k => f' (k + nf j * ntrd)) 0 (perm_n ntrd)).
 Proof.
   assert (Hgi_nt: gi i j < nt_gr) by eauto.
   unfold stencil.
@@ -376,4 +367,269 @@ Proof.
   sep_rewrite mps_eq2; [|exact Heq]; clear Heq.
   repeat sep_cancel.
 Qed.
-Section Stencil_seq.
+
+Require Import Bdiv.
+
+Lemma is_array_distr e n (f' : nat -> Z) :
+  forall m s stk,
+    stk ||= conj_xs (ls_init m n (fun i => e +o Zn (i + s) -->p (1, f' (i + s)))) <=>
+        is_array e n f' (m + s).
+Proof.
+  induction n; intros m s stk; simpl.
+  - reflexivity.
+  - rewrite IHn; reflexivity.
+Qed.
+
+Lemma is_array_distr0 e n (f' : nat -> Z) :
+  forall s stk,
+    stk ||= conj_xs (ls_init 0 n (fun i => e +o Zn (i + s) -->p (1, f' (i + s)))) <=>
+        is_array e n f' s.
+Proof.
+  lets: (is_array_distr e n f' 0); simpl in *; eauto.
+Qed.
+
+Lemma is_array_distr00 e n (f' : nat -> Z) :
+  forall stk,
+    stk ||= conj_xs (ls_init 0 n (fun i => e +o Zn i -->p (1, f' i))) <=>
+        is_array e n f' 0.
+Proof.
+  lets: (is_array_distr0 e n f' 0); simpl in *.
+  erewrite ls_init_eq0 in H.
+  2: intros; rewrite <-plus_n_O; reflexivity.
+  eauto.
+Qed.
+
+Lemma is_array_change (e : loc_exp) (f1 f2 : nat -> Z) n :
+  forall s, (forall x, x < n -> f1 (x + s) = f2(x + s)) ->
+            forall stc,
+              stc ||= is_array e n f1 s <=> is_array e n f2 s.
+Proof.
+  induction n; simpl; intros s Hf; try reflexivity.
+  intros stc; rewrite IHn.
+  cutrewrite (f1 s = f2 s); [reflexivity|].
+  pose proof (Hf 0); rewrite plus_O_n in H; rewrite H; omega.
+  intros x Hx; rewrite <-Nat.add_succ_comm; apply Hf; omega.
+Qed.
+
+Lemma is_array_concat e f' len1 len2 : forall s stc,
+    (* (forall off, off < len1 + len2 -> *)
+    (*  f' (s + off) = if lt_dec off len1 then f1 (s + off) else f2 (s + off)) -> *)
+    stc||=
+       is_array e (len1 + len2) f' s <=>
+       is_array e len1 f' s ** is_array e len2 f' (s + len1).
+Proof.
+  induction len1; simpl; intros s stc.
+  - rewrite emp_unit_l, <-plus_n_O.
+    rewrite is_array_change; [reflexivity|..].
+    intros; rewrite plus_comm; eauto.
+  - intros. rewrite <-Nat.add_succ_comm, <-sep_assoc, IHlen1.
+    reflexivity.
+Qed.
+
+Ltac istar_simplify_eq :=
+  rewrite sc_v2l; rewrite (vec_to_list_init0 _ emp); erewrite ls_init_eq0;
+   [ idtac
+   | let i := fresh "i" in
+     let Hi := fresh in
+     let Hex := fresh in
+     let Heq := fresh in
+     intros i Hi;
+      lazymatch goal with
+      | |- match ?X with
+           | inleft _ => _
+           | inright _ => _
+           end = _ => destruct X as [| Hex] eqn:Heq; [ idtac | destruct Hex; omega ]
+      end; rewrite (Fin_nat_inv Heq); reflexivity ]; 
+   match goal with
+   | _ => idtac
+   end.
+
+Lemma dist_for_stencil l (f' : nat -> Z) stk : 
+  stk ||= Aistar_v (MyVector.init
+             (fun (i : Fin.t ntrd) => (Sh l +o Zn (nf i + 1) -->p (1,  f' (nf i + 1))) **
+               (if Nat.eq_dec (nf i) 0 then Sh l -->p (1, f' 0) else emp) **
+               (if Nat.eq_dec (nf i) (ntrd - 1) then (Sh l +o Zn (ntrd + 1)) -->p (1, f' (ntrd + 1)) else emp))) <=>
+      is_array (Sh l) (ntrd + 2) f' 0.
+Proof.
+  istar_simplify_eq.
+  cutrewrite (ntrd + 2 = 1 + ntrd + 1); [|ring].
+  repeat rewrite is_array_concat.
+  cutrewrite (0 + (1 + ntrd) = ntrd + 1); [|ring]; simpl.
+  rewrite !emp_unit_r, <-!sep_assoc, !ls_star.
+  rewrite is_array_distr0.
+
+  rewrite <-(firstn_skipn 1 (ls_init _ _ _)), firstn_init, skipn_init, conj_xs_app.
+  rewrite min_l; try omega.
+  erewrite ls_init_eq'.
+  2: intros; destruct Nat.eq_dec; try omega.
+  2: lazymatch goal with [|- ?X = _ ?i] => cutrewrite (X = id (fun _ : nat => X) i); eauto end.
+  unfold id; simpl; rewrite emp_unit_r.
+  erewrite ls_init_eq'.
+  2: intros; destruct Nat.eq_dec; try omega.
+  2: lazymatch goal with [|- ?X = _ ?i] => cutrewrite (X = id (fun _ : nat => X) i); eauto end.
+  rewrite init_emp_emp, emp_unit_r.
+
+  rewrite <-(firstn_skipn (ntrd - 1) (ls_init _ _ _)), firstn_init, skipn_init, conj_xs_app.
+  rewrite min_l; try omega.
+  erewrite ls_init_eq'.
+  2: intros; destruct Nat.eq_dec; try omega.
+  2: lazymatch goal with [|- ?X = _ ?i] => cutrewrite (X = id (fun _ : nat => X) i); eauto end.
+  cutrewrite (ntrd - (ntrd - 1) = 1); [|omega]; simpl.
+  unfold id; simpl; rewrite init_emp_emp, !emp_unit_l, !emp_unit_r.
+  destruct Nat.eq_dec; try omega.
+  split; intros; repeat sep_cancel.
+Qed.  
+
+Lemma BS_ok j n: Aistar_v (fst (BS j n)) |= Aistar_v (snd (BS j n)).
+Proof.
+  unfold BS, BS0; destruct n; simpl; eauto.
+  intros s h H.
+  apply sc_v2l; rewrite (vec_to_list_init0 _ emp); erewrite ls_init_eq0.
+  2: intros i Hi; destruct Fin.of_nat as [|[? ?]]; try omega.
+  2: lazymatch goal with [|- ?X = _ ?i] => cutrewrite (X = id (fun _ : nat => X) i); eauto end.
+  unfold id.
+  sep_rewrite_r is_array_is_array_p_1; eauto.
+  apply dist_for_stencil; eauto; simpl.
+  istar_simplify.
+  istar_simplify_in H.
+  repeat sep_rewrite (@ls_star ntrd).
+  repeat sep_cancel.
+  erewrite ls_init_eq0; [apply scC; erewrite ls_init_eq0; [apply scC|]|]; eauto.
+  intros; destruct Nat.eq_dec; do 3 f_equal; eauto; nia.
+  intros; simpl; do 3 f_equal; omega.
+Qed.
+
+Lemma precise_pts : forall (e1 : loc_exp) (e2 : exp) (q : Qc), precise (e1 -->p (q,  e2)).
+Proof.
+  intros; unfold precise; simpl; intros; unfold_conn_all.
+  destruct h1, h1'; apply pheap_eq; extensionality x; simpl in *.
+  rewrite H, H0; destruct (eq_dec _ _); eauto.
+Qed.
+
+Lemma precise_is_array_p e len s f' p : precise (is_array_p e len f' s p).
+Proof.
+  revert s; induction len; simpl; intros; eauto using precise_emp, precise_star, precise_pts.
+Qed.  
+
+Lemma precise_BS b j i :
+  precise (Vector.nth (fst (BS j b)) i) /\
+  precise (Vector.nth (snd (BS j b)) i).
+Proof.
+  split; unfold BS, BS0; destruct b; simpl; rewrite MyVector.init_spec;
+    try now idtac.
+  - repeat destruct Nat.eq_dec; repeat apply precise_star; eauto using precise_pts, precise_emp, precise_is_array_p.
+  - repeat destruct Nat.eq_dec; repeat apply precise_star; eauto using precise_pts, precise_emp, precise_is_array_p.
+Qed.
+
+Definition Pre_ij (j : Fin.t nblk)  (i : Fin.t ntrd) :=
+  !("bid" === Zn (nf j)) **
+  !("arr" === aloc) **
+  !("out" === oloc) **
+  !("smem" === sloc ) **
+  is_array_p (Gl aloc) nt_gr f 0 (perm_n nt_gr) **
+  (Gl oloc +o Zn (nf i + nf j * ntrd) -->p (1, f_out (nf i + nf j * ntrd))) **
+  (Sh sloc +o Zn (nf i + 1) -->p (1, f_sm (nf i + 1))) **
+  (if Nat.eq_dec (nf i) 0 then Sh sloc -->p (1, f_sm 0) else emp) **
+  (if Nat.eq_dec (nf i) (ntrd - 1) then Sh sloc +o Zn (ntrd + 1) -->p (1, f_sm (ntrd + 1)) else emp).
+
+Definition Post_ij (j : Fin.t nblk) (i : Fin.t ntrd) :=
+  (is_array_p (Gl aloc) nt_gr f 0 (perm_n nt_gr) **
+  (Gl oloc +o Zn (gi i j) -->p (1, (f' (gi i j) + f' (gi i j + 1) + f' (gi i j + 2)))%Z) **
+  is_array_p (Sh sloc) (ntrd + 2) (fun k => f' (k + nf j * ntrd)) 0 (perm_n ntrd)).
+
+Definition Pre_j (j : Fin.t nblk) :=
+  !("arr" === aloc) **
+  !("out" === oloc) **
+  !("smem" === sloc ) **
+  conj_xs (ls_init 0 ntrd (fun (i : nat) => is_array_p (Gl aloc) nt_gr f 0 (perm_n nt_gr))) **
+  conj_xs (ls_init 0 ntrd (fun (i : nat) =>
+                             (Gl oloc +o Zn (i + nf j * ntrd) -->p (1, f_out (i + nf j * ntrd))))).
+
+Definition Post_j (j : Fin.t nblk) :=
+  conj_xs (ls_init 0 ntrd (fun i : nat => (is_array_p (Gl aloc) nt_gr f 0 (perm_n nt_gr)))) **
+  conj_xs (ls_init 0 ntrd (fun i : nat =>
+    (Gl oloc +o Zn (i + nf j * ntrd) -->p (1, (f' (i + nf j * ntrd) + f' (i + nf j * ntrd + 1) + f' (i + nf j * ntrd + 2)))%Z))).
+
+Definition E (v : var) :=
+  if var_eq_dec v "tid" then Hi
+  else if var_eq_dec v "t0" then Hi
+  else if var_eq_dec v "t1" then Hi
+  else if var_eq_dec v "t2" then Hi
+  else Lo.
+
+Lemma stencil_ok_bl (j : Fin.t nblk) :
+  CSLp ntrd E
+       (!("bid" === Zn (nf j)) ** is_array (Sh sloc) (ntrd + 2) f_sm 0 ** Pre_j j)
+       stencil
+       (is_array (Sh sloc) (ntrd + 2) (fun k => f' (k + nf j * ntrd)) 0 ** Post_j j).
+Proof.
+  applys (>>(@rule_par) (BS j) (MyVector.init (Pre_ij j)) (MyVector.init (Post_ij j))).
+  - destruct ntrd; eexists; try omega; eauto.
+  - unfold BS, BS0; intros n; split; intros; destruct n; simpl; rewrite MyVector.init_spec; simpl; unfold CSL.low_assn;
+      repeat prove_low_assn; repeat econstructor; [equates 1; [repeat econstructor|]..].
+    repeat instantiate (1 := Lo); eauto.
+    repeat instantiate (1 := Lo); eauto.
+  - eauto using BS_ok.
+  - eauto using precise_BS.
+  - unfold Pre_j, Pre_ij; intros s h H; istar_simplify.
+    repeat first [sep_rewrite (@ls_star ntrd) | sep_rewrite (@ls_pure ntrd)].
+    sep_split_in H; sep_split;
+      try (apply ls_emp'; rewrite init_length; intros; rewrite ls_init_spec; destruct lt_dec; try omega; eauto).
+    repeat sep_cancel.
+    apply dist_for_stencil in H1; istar_simplify_in H1; eauto.
+  - unfold Post_j, Post_ij; intros s h H; istar_simplify_in H.
+    repeat sep_cancel.
+    sep_rewrite_in_r is_array_is_array_p_1 H0; repeat sep_cancel.
+  - intros; unfold CSL.low_assn, Pre_ij; rewrite MyVector.init_spec; repeat prove_low_assn; repeat econstructor;
+      [equates 1; [repeat econstructor|]..].
+    repeat (instantiate (1 := Lo)); eauto.
+    repeat (instantiate (1 := Lo)); eauto.
+    repeat (instantiate (1 := Lo)); eauto.
+  - intros; unfold CSL.low_assn, Post_ij; rewrite MyVector.init_spec; repeat prove_low_assn; repeat econstructor;
+      [equates 1; [repeat econstructor|]..].
+    repeat (instantiate (1 := Lo)); eauto.
+    repeat (instantiate (1 := Lo)); eauto.
+  - unfold E, stencil.
+    instantiate (1 := Lo).
+    repeat econstructor; simpl; repeat (repeat instantiate (1 := Hi); auto).
+    repeat instantiate (1 := Hi); auto.    
+    repeat instantiate (1 := Hi); auto.
+    repeat instantiate (1 := Hi); auto.
+    repeat instantiate (1 := Hi); auto.
+  - intros; eapply rule_conseq; eauto using stencil_ok_th;
+    unfold Pre_ij, Post_ij; rewrite MyVector.init_spec; intros; sep_normal_in H; sep_normal; repeat sep_cancel.
+    Grab Existential Variables.
+    exact 0.
+Qed.
+
+Lemma stencil_ok_gr :
+  CSLg _ _ ntrd_neq_0 nblk_neq_0
+       (!("arr" === aloc) **
+        !("out" === oloc) **
+        !("smem" === sloc ) **
+        is_array (Gl aloc) nt_gr f 0 **
+        is_array (Gl oloc) nt_gr f_out 0)
+       {| get_sh_decl := (Var "smem", ntrd+2) :: nil; get_cmd := stencil |}
+       (!("arr" === aloc) **
+        !("out" === oloc) **
+        !("smem" === sloc ) **
+        is_array (Gl aloc) nt_gr f 0 **
+        is_array (Gl oloc) nt_gr (fun i => (f' i + f' (i + 1) + f' (i + 2))%Z) 0).
+Proof.
+  applys (>>rule_grid E (MyVector.init Pre_j) (MyVector.init Post_j)).
+  - unfold Pre_j; intros.
+    istar_simplify.
+    repeat first [sep_rewrite (@ls_star nblk) | sep_rewrite (@ls_pure nblk)].
+    sep_split_in H; sep_split;
+      try (apply ls_emp'; rewrite init_length; intros; rewrite ls_init_spec; destruct lt_dec; try omega; eauto).
+    sep_rewrite (conj_xs_init_flatten0 nblk ntrd (fun v => Gl oloc +o Zn v -->p (1, f_out v))).
+    sep_rewrite (conj_xs_init_flatten0 nblk ntrd (fun v => is_array_p (Gl aloc) nt_gr f 0 (perm_n nt_gr))).
+    sep_rewrite_r is_array_is_array_p_1; try nia.
+    sep_rewrite is_array_distr00; try nia.
+    eauto.
+  - intros; eapply CSLp_backward; [eapply CSLp_forward; [apply (stencil_ok_bl bid)|]|];
+      unfold Pre_j, Post_j; rewrite MyVector.init_spec; simpl; intros s h H.
+    + sep_rewrite emp_unit_r; apply ex_lift_l; eexists.
+      repeat sep_cancel; eauto.
+
+End Stencil.
