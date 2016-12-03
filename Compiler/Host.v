df939e4839736da59f3cafb44a5b89194d997ec4
Izumi ASAKURA <i.asakura58@gmail.com>
2016-12-04 07:12:47 +0900

    Prove the funcall rule

--- Compiler/Host.v
+++ Compiler/Host.v
@@ -1,30 +1,32 @@
-Require Import LibTactics GPUCSL Grid Relations MyEnv.
+Require Import LibTactics GPUCSL Grid Relations MyEnv CSLLemma.
 
 (* Add kernel name *)
 Record kernel := BuildKer { (* name : string; *) params_of : list (var * CTyp); body_of : program }.
 
-Inductive expr :=
-| VarE (x : var)
-| Const (n : Z)
-| Min (e1 e2 : expr)
-| Add (e1 e2 : expr)
-| Sub (e1 e2 : expr)
-| Div (e1 e2 : expr).
-
-Definition kerID := nat.
-Instance nat_eq : eq_type nat := {|eq_dec := Nat.eq_dec|}.
+(* Inductive expr := *)
+(* | VarE (x : var) *)
+(* | Const (n : Z) *)
+(* | Min (e1 e2 : expr) *)
+(* | Add (e1 e2 : expr) *)
+(* | Sub (e1 e2 : expr) *)
+(* | Div (e1 e2 : expr). *)
+(* Definition kerID := nat. *)
+(* Instance nat_eq : eq_type nat := {|eq_dec := Nat.eq_dec|}. *)
 
 (* the syntax of host-side program *)
 Inductive stmt :=
 | host_skip : stmt
-| host_alloc : var -> expr -> stmt
-| host_iLet : var -> expr -> stmt
-| host_invoke : string -> nat -> nat -> list expr -> stmt
+| host_alloc : var -> exp -> stmt
+| host_iLet : var -> exp -> stmt
+| host_invoke : string -> nat -> nat -> list exp -> stmt
 (* runtime expression representing kernel execution *)
 | host_exec_ker : forall ntrd nblk,
     Vector.t (klist ntrd) nblk
     -> Vector.t simple_heap nblk
     -> stmt
+| host_call : string -> list exp -> stmt
+(* runtime expression for executing another function *)
+| host_exec_hfun : stmt -> stack -> stmt
 | host_seq : stmt -> stmt -> stmt.
 
 Record hostfun := Hf {
@@ -42,9 +44,9 @@ Definition fdecl : Type := string * fd.
 (* a program consists of several host/kernel function declarations *)
 Definition GModule := list fdecl.
 
-Record GPUstate := GS {
-  gs_stack : stack;
-  gs_heap : simple_heap
+Record State := St {
+  st_stack : stack;
+  st_heap : simple_heap
 }.
 
 Definition alloc_heap (start len : nat) : simple_heap :=
@@ -68,15 +70,15 @@ Definition lift {A B C : Type} (f : A -> B -> C) x y :=
   | _, _ => None
   end.
 
-Fixpoint eval_expr (env : stack) (e : expr) : Z :=
-  match e with
-  | VarE x => henv_get env x
-  | Const c => c
-  | Min e1 e2 => Z.min (eval_expr env e1) (eval_expr env e2)
-  | Add e1 e2 => Z.add (eval_expr env e1) (eval_expr env e2)
-  | Div e1 e2 => Z.div (eval_expr env e1) (eval_expr env e2)
-  | Sub e1 e2 => Z.sub (eval_expr env e1) (eval_expr env e2)
-  end.
+(* Fixpoint eval_expr (env : stack) (e : expr) : Z := *)
+(*   match e with *)
+(*   | VarE x => henv_get env x *)
+(*   | Const c => c *)
+(*   | Min e1 e2 => Z.min (eval_expr env e1) (eval_expr env e2) *)
+(*   | Add e1 e2 => Z.add (eval_expr env e1) (eval_expr env e2) *)
+(*   | Div e1 e2 => Z.div (eval_expr env e1) (eval_expr env e2) *)
+(*   | Sub e1 e2 => Z.sub (eval_expr env e1) (eval_expr env e2) *)
+(*   end. *)
 
 Fixpoint func_disp (m : GModule) (name : string) :=
   match m with
@@ -99,19 +101,19 @@ Instance var_eq_type : eq_type var := {|
   eq_dec := var_eq_dec
 |}.
 
-Inductive stmt_exec : GModule -> stmt -> GPUstate -> stmt -> GPUstate -> Prop :=
-| Exec_alloc kenv (x : var) e (gst : GPUstate) start n :
-    eval_expr (gs_stack gst) e = (Z.of_nat n) ->
-    hdisj (gs_heap gst) (alloc_heap start n) ->
+Inductive stmt_exec : GModule -> stmt -> State -> stmt -> State -> Prop :=
+| Exec_alloc kenv (x : var) e (gst : State) start n :
+    edenot e (st_stack gst) = (Z.of_nat n) ->
+    hdisj (st_heap gst) (alloc_heap start n) ->
     stmt_exec kenv (host_alloc x e) gst
-              host_skip (GS (upd (gs_stack gst) x (Z.of_nat start))
-                            (hplus (gs_heap gst) (alloc_heap start n)))
-| Exec_iLet kenv x e (gst : GPUstate) n :
-    eval_expr (gs_stack gst) e = n ->
+              host_skip (St (upd (st_stack gst) x (Z.of_nat start))
+                            (hplus (st_heap gst) (alloc_heap start n)))
+| Exec_iLet kenv x e (gst : State) n :
+    edenot e (st_stack gst) = n ->
     stmt_exec kenv (host_iLet x e) gst
-              host_skip (GS (upd (gs_stack gst) x n) (gs_heap gst))
+              host_skip (St (upd (st_stack gst) x n) (st_heap gst))
 | Exec_invoke ntrd nblk kenv tst shp kerID ker args vs gst stk :
-    List.Forall2 (fun a v => eval_expr (gs_stack gst) a = v) args vs ->
+    List.Forall2 (fun a v => edenot a (st_stack gst) = v) args vs ->
     func_disp kenv kerID = Some (Kern ker) ->
     init_GPU ntrd nblk (body_of ker) tst shp stk ->
     bind_params stk (params_of ker) vs ->
@@ -121,13 +123,29 @@ Inductive stmt_exec : GModule -> stmt -> GPUstate -> stmt -> GPUstate -> Prop :=
 | Exec_invoke_step kenv ntrd nblk tst tst' shp shp' s h h' :
     red_g (Gs tst shp h)  (Gs tst' shp' h') ->
     stmt_exec kenv
-              (host_exec_ker ntrd nblk tst shp) (GS s h)
-              (host_exec_ker ntrd nblk tst' shp') (GS s h')
+              (host_exec_ker ntrd nblk tst shp) (St s h)
+              (host_exec_ker ntrd nblk tst' shp') (St s h')
 | Exec_invoke_end kenv ntrd nblk tst shp s h :
     (forall i j, fst tst[@j][@i] = Cskip) ->
     stmt_exec kenv
-              (host_exec_ker ntrd nblk tst shp) (GS s h)
-              (host_skip) (GS s h)
+              (host_exec_ker ntrd nblk tst shp) (St s h)
+              (host_skip) (St s h)
+| Exec_call kenv st fn hf args vs new_stk :
+    func_disp kenv fn = Some (Host hf)
+    -> List.Forall2 (fun a v => edenot a (st_stack st) = v) args vs
+    -> bind_params new_stk (host_params hf) vs
+    -> stmt_exec kenv 
+                 (host_call fn args) st
+                 (host_exec_hfun (host_stmt hf) (st_stack st)) (St new_stk (st_heap st))
+| Exec_hfun_step kenv body1 body2 st1 st2 ret_st :
+    stmt_exec kenv body1 st1 body2 st2
+    -> stmt_exec kenv
+                 (host_exec_hfun body1 ret_st) st1
+                 (host_exec_hfun body2 ret_st) st2
+| Exec_hfun_end kenv ret_st st :
+    stmt_exec kenv
+              (host_exec_hfun host_skip ret_st) st
+              host_skip (St ret_st (st_heap st))
 | Exec_seq1 kenv s1 s2 s1' st1 st2 :
     stmt_exec kenv s1 st1 s1' st2  ->
     stmt_exec kenv (host_seq s1 s2) st1 (host_seq s1' s2) st2
@@ -137,7 +155,7 @@ End VecNot.
 
 Inductive aborts_h : GModule -> stmt -> stack -> simple_heap -> Prop :=
   | aborts_host_seq : forall ke p p' s h, aborts_h ke p s h -> aborts_h ke (host_seq p p') s h
-  | aborts_kernel_call : forall ke kid n m args s h,
+  | aborts_kernel_invk : forall ke kid n m args s h,
       func_disp ke kid = None \/
       (exists f, func_disp ke kid = Some (Host f)) \/
       n = 0 \/ m = 0 \/
@@ -145,7 +163,15 @@ Inductive aborts_h : GModule -> stmt -> stack -> simple_heap -> Prop :=
       aborts_h ke (host_invoke kid n m args) s h
   | aborts_kernel_exec : forall kenv ntrd nblk tst shp s h,
       (abort_g (Gs tst shp h) \/ bdiv_g ntrd nblk tst shp (htop h)) ->
-      aborts_h kenv (host_exec_ker ntrd nblk tst shp) s h.
+      aborts_h kenv (host_exec_ker ntrd nblk tst shp) s h
+  | aborts_hfun_call : forall ke fn args s h,
+      func_disp ke fn = None \/
+      (exists f, func_disp ke fn = Some (Kern f)) \/
+      (forall hf, func_disp ke fn = Some (Host hf) -> length args <> length (host_params hf)) ->
+      aborts_h ke (host_call fn args) s h
+  | aborts_hfun_exec : forall kenv body ret_stk s h,
+      aborts_h kenv body s h
+      -> aborts_h kenv (host_exec_hfun body ret_stk) s h.
 
 Notation zpheap := (gen_pheap Z).
 
@@ -161,27 +187,26 @@ Fixpoint safe_nh (n : nat) (s : stack) (gh : zpheap) (p : stmt) (Q : assn) :=
     (forall (hF : zpheap) (h' : simple_heap),
         pdisj gh hF 
         -> ptoheap (phplus gh hF) h'
-        -> aborts_h GM p s h') /\
+        -> ~ aborts_h GM p s h') /\
     (forall (hF : zpheap) (h h' : simple_heap) (s' : stack) (p' : stmt),
         pdisj gh hF 
         -> ptoheap (phplus gh hF) h'
-        -> stmt_exec GM p (GS s h) p' (GS s' h') 
+        -> stmt_exec GM p (St s h) p' (St s' h') 
         -> exists (h'' : zpheap),
             pdisj h'' hF /\ ptoheap (phplus h'' hF) h' /\
-            safe_nh n s h'' p' Q)
+            safe_nh n s' h'' p' Q)
   end.
 
 Definition CSLh_n_simp (P : assn) (ss : stmt) (Q : assn) (n : nat) :=
   forall s h,
-    P s (as_gheap h) -> safe_nh n s h ss Q.
+    sat s (as_gheap h) P -> safe_nh n s h ss Q.
 
 Definition CSLh_simp P ss Q := forall n, CSLh_n_simp P ss Q n.
 
 Definition CSLhfun_n_simp (P : assn) (f : hostfun) (Q : assn) (n : nat) :=
   forall vs s h,
-    length vs = length (host_params f)
-    -> bind_params s (host_params f) vs
-    -> P s (as_gheap h)
+    bind_params s (host_params f) vs
+    -> sat s (as_gheap h) P
     -> safe_nh n s h (host_stmt f) Q.
 
 Definition CSLhfun_simp P f Q := forall n, CSLhfun_n_simp P f Q n.
@@ -191,7 +216,7 @@ Definition CSLgfun_n_simp (P : assn) (f : kernel) (Q : assn) (n : nat) :=
     ntrd <> 0 -> nblk <> 0
     -> init_GPU ntrd nblk (body_of f) tst shs s
     -> bind_params s (params_of f) vs
-    -> P s (as_gheap h)
+    -> sat s (as_gheap h) P
     -> safe_ng ntrd nblk n tst shs h Q.
 
 Definition CSLgfun_simp P f Q := forall n, CSLgfun_n_simp P f Q n.
@@ -311,7 +336,199 @@ Proof.
   unfold interp_prog_n, CSLgfun_n_simp, CSLg_n in *.
   revert Hok.
   induction fs; simpl; eauto.
-Qed.  
+Qed.
+
+Inductive inst_spec : FSpec -> assn -> assn -> Prop :=
+| IS_dbl P Q : inst_spec (FDbl P Q) P Q 
+| IS_all T (v : T) f P Q : inst_spec (f v) P Q -> inst_spec (FAll _ f) P Q.
+
+(* (y = v)[vs/xs], if variable remains in sustituted pred, it cannot holds *)
+Fixpoint subst_ent (y : var) (v : val) (xs : list var) (vs : list val) : Prop :=
+  match xs, vs with
+  | x :: xs, v' :: vs => if var_eq_dec x y then v = v' else subst_ent y v xs vs
+  | _, _ => False
+  end.
+
+Fixpoint subst_env (E : list entry) xs vs : Prop :=
+  match E with
+  | nil => True
+  | (y |-> v') :: E => subst_ent y v' xs vs /\ subst_env E xs vs
+  end.
+
+Lemma interp_fs_inst fs k P Q :
+  interp_fs fs k 
+  -> inst_spec fs P Q 
+  -> k P Q.
+Proof.
+  intros Hint Hinst; induction Hinst; simpl in *; eauto.
+Qed.
+
+Lemma subst_ent_bind_params y v xs vs s :
+  subst_ent y v (map fst xs) vs
+  -> bind_params s xs vs 
+  -> (y === v) s (emp_ph loc).
+Proof.
+  revert vs; induction xs as [|[x ?] xs]; simpl; try tauto.
+  intros [|v' vs]; try tauto.
+  destruct var_eq_dec; substs; jauto.
+  intros ? [? ?]; cbv; congruence.
+Qed.
+
+Lemma subst_env_bind_params E xs vs s : 
+  subst_env E (map fst xs) vs
+  -> bind_params s xs vs
+  -> env_assns_denote E s (emp_ph loc).
+Proof.
+  revert xs vs s; induction E as [| [y v'] E]; introv; simpl.
+  - intros; apply emp_emp_ph.
+  - intros; split.
+    + applys* subst_ent_bind_params.
+    + applys* IHE.
+Qed.
+
+Lemma phplus_gheap_comm (h1 : zpheap) (h2: zpheap) (dis : pdisj h1 h2) :
+  phplus (as_gheap h1) (as_gheap h2) = as_gheap (phplus_pheap dis).
+Proof.
+  extensionality l; simpl; unfold phplus, as_gheap'.
+  destruct l as [[|] ?]; eauto.
+Qed.
+
+Lemma safe_nh_frame n s (h hF : zpheap) ss R RF P E (dis : pdisj h hF) : 
+  sat_res s (as_gheap hF) RF
+  -> safe_nh n s h ss (Assn R P E)
+  -> safe_nh n s (phplus_pheap dis) ss (Assn (R *** RF) P E).
+Proof.
+  revert s ss h hF dis; induction n; introv; simpl; eauto.
+  intros HsatF (Hskip & Hsafe & Hstep); splits; eauto.
+  - intros; forwards* Hsk: Hskip.
+    unfold Assn in * ;sep_split; sep_split_in Hsk; jauto.
+    exists (as_gheap h) (as_gheap hF); splits; eauto; eauto.
+    + apply* as_gheap_pdisj.
+    + apply (phplus_gheap_comm _ _ dis).
+  - introv Hdis' Heq' Hab.
+    rewrite padd_assoc in Heq'.
+    rewrites* pdisj_padd_expand in Hdis'; eauto.
+    destruct Hdis'.
+    forwards*: (>>Hsafe (phplus_pheap H0)).
+  - introv Hdis' Heq' Hstep'.
+    rewrite padd_assoc in Heq'.
+    rewrites* pdisj_padd_expand in Hdis'; eauto.
+    destruct Hdis'.
+    forwards* (h'' & Hdis'' & Heq'' & ?): (>>Hstep (phplus_pheap H0)).
+    simpl in *.
+    assert (Hdis''F : pdisj h'' hF) by eauto using pdisjE1.
+    exists (phplus_pheap Hdis''F); splits; eauto using pdisj_padd_expand.
+    + simpl; applys* pdisj_padd_expand.
+    + simpl; rewrites* padd_assoc.
+    + applys* IHn.
+      unfold sat_res, sat in *; eauto using res_inde.
+Qed.
+
+Lemma as_gheap_pdisj (h1 h2 : zpheap) :
+  pdisj h1 h2 <-> pdisj (as_gheap h1) (as_gheap h2).
+Proof.
+  split; eauto using as_gheap_pdisj.
+  unfold pdisj, as_gheap; intros H l.
+  forwards*: (H (GLoc l)).
+Qed.
+
+Lemma safe_nh_exec_hfun n s h body s_ret R P E E' :
+  safe_nh n s h body (Assn R P E)
+  -> env_assns_denote E' s_ret (emp_ph loc)
+  -> safe_nh n s h (host_exec_hfun body s_ret) (Assn R P E').
+Proof.
+  revert s h body; induction n; simpl; introv; eauto.
+  intros (Hskip & Hsafe & Hstep) Henv; splits; eauto.
+  - inversion 1.
+  - introv Hdis Heq Hab.
+    inverts Hab.
+    forwards*: Hsafe.
+  - introv Hdis Heq Hst.
+    inverts Hst as Hst'.
+    + forwards* (h'' & ? & ? & ?): Hstep.
+    + exists h; splits; eauto.
+      destruct n; simpl; eauto; splits; eauto.
+      * intros _.
+        forwards* Hsat: Hskip.
+        unfold Assn in Hsat |- * ;sep_split; sep_split_in Hsat; eauto.
+      * introv ? ? Hab; inverts Hab.
+      * introv ? ? Hst; inverts Hst.
+Qed.
+
+Lemma safe_nh_conseq n s h body P P' :
+  P |= P' 
+  -> safe_nh n s h body P
+  -> safe_nh n s h body P'.
+Proof.
+  revert s h body; induction n; simpl; eauto; introv.
+  intros ? (? & ? & ?); splits; jauto.
+  - intros; apply H; eauto.
+  - intros; forwards* (? & ? & ? & ?): H2.
+Qed.
+
+Lemma rule_call (G : FC) (fn : string) (nt nb : nat) (es : list exp)
+      xs vs body res
+      fs 
+      Rpre Ppre Epre
+      Rpst Ppst
+      RF R E (P : Prop) :
+  fc_ok G = true
+  -> In (fn, fs) G
+  -> func_disp GM fn = Some (Host (Hf xs body res))
+  -> length es = length xs
+  -> inst_spec fs (Assn Rpre Ppre Epre) (Assn Rpst Ppst nil)
+  -> List.Forall2 (fun e v => evalExp E e v) es vs
+  -> (P -> subst_env Epre (map fst xs) vs)
+  -> (P -> Ppre)
+  -> (P -> R |=R Rpre *** RF)
+  -> CSLh_n G
+            (Assn R P E)
+            (host_call fn es)
+            (Assn (Rpst *** RF) (P /\ Ppst) E).
+Proof.
+  intros Hfcok Hinfn Hdisp Harg Hinst Heval Hsubst Hp Hr n HFC s h Hsat.
+  destruct n; simpl; eauto.
+  splits; eauto.
+  - inversion 1.
+  - introv Hdisj Htoh Habort.
+    inverts Habort as Habort.
+    destruct Habort as [? | [ [? ?] | Hcallab] ]; try congruence.
+    forwards* Hc: (>> Hcallab Hdisp); simpl in Hc.
+  - introv Hdis Htoh Hstep.
+    simpl in HFC; rewrite <-minus_n_O in HFC.
+    unfold interp_FC_n, interp_htri_n in HFC; rewrite Forall_forall in HFC.
+    forwards* Hfn: (>>HFC Hinfn); rewrite Hdisp in Hfn.
+    forwards* Hfn': (>>interp_fs_inst Hfn Hinst); simpl in Hfn'.
+    unfold CSLhfun_n_simp in Hfn'; simpl in Hfn'.
+    inverts Hstep as Hdisp' Heval' Hbnd; simpl in *.
+    rewrite Hdisp in Hdisp'; inverts Hdisp'; simpl in *.
+    unfold Assn, sat in Hsat.
+    sep_split_in Hsat.
+    forwards* (h1 & h2 & Hpre & HF & Hdis12 & Heq12): (>> Hr HP Hsat).
+    forwards* (h1' & h2' & ? & ? & Heq12'): (>> phplus_gheap  Heq12); substs.
+    forwards* Hsafe: (>>Hfn' h1' Hbnd).
+    (* Proof that precond holds *)
+    { unfold sat, Assn; sep_split; eauto.
+      - applys* Hp.
+      - applys* subst_env_bind_params.
+        cutrewrite (vs = vs0); eauto.
+        revert Heval Heval' HP0; clear.
+        intros H; revert vs0; induction H; inversion 1; intros; substs; eauto.
+        forwards*: evalExp_ok.
+        f_equal; eauto. }
+    (* h **                                     hF -> 
+       h1' ** (h2' : framed w.r.t. fun exec. ) ** hF -> *)
+    exists h; splits; eauto.
+    rewrite <-as_gheap_pdisj in Hdis12.
+    asserts_rewrite (h = phplus_pheap Hdis12).
+    { destruct h; apply pheap_eq; eauto. }
+    applys* safe_nh_frame.
+    { unfold sat, sat_res in *; now eauto using res_inde. }
+    applys* (>>safe_nh_exec_hfun (@nil entry)).
+    applys* (>>safe_nh_conseq Hsafe).
+    applys* Assn_imply.
+    unfold incl; eauto.
+Qed.    
 
 (* Fixpoint assn_of_bind (params : list var) (args : list Z) := *)
 (*   match params, args with *)
