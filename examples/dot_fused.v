77656e22731452414be03cd034153c39fcc86c7a
Izumi Asakura <i.asakura58@gmail.com>
2018-04-09 16:52:54 +0900

    Remove unused code

--- examples/dot_fused.v
+++ examples/dot_fused.v
@@ -5,15 +5,6 @@ Open Scope Z_scope.
 Require Import DepList.
 Import Skel.
 
-Definition dot_fused_IR : AS (TZ :: TZ :: nil) TZ :=
-  ALet _ _ _ (
-    Reduce _ _  (F2 _ TZ TZ TZ (EBin _ _ _ _ _ Eplus (EVar _ _ _ (HNext HFirst)) (EVar _ _ _ HFirst)))
-      (DArr _ _ (F1 _ TZ TZ (EBin _ _ _ _ _ Emult 
-                                  (EA _ _ _ (HNext HFirst) (EVar _ _ _ HFirst) )
-                                  (EA _ _ _ HFirst (EVar _ _ _ HFirst)))) (LLen _ _ HFirst))
-  ) (
-  ARet _ _ HFirst).
-
 Definition dot (xs ys : list Z) : comp (list Z)
   := do! t <- mapM (fun xy => ret (fst xy * snd xy)) (zip xs ys) in
      reduceM (fun x y => ret (x + y)) t.
@@ -37,11 +28,6 @@ Proof.
   - intros; omega.
   - intros; omega.
 Defined.
-
-Goal False.
-  pose (asDenote _ _ dot_fused_IR) as D.
-  simpl in D.
-Abort.
 
 Definition res := save_to_file (`dot_fused_GPGPU) "./dot_fused.cu".
 
